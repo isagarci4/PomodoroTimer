3009a2800ab84f3d2f5a0884706d14d1a019af3c
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2024-01-15 15:25:41 -0300

    feat: configurando ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
 
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
-     <Button variant="secondary" />
-     <Button variant="success" />
-     <Button variant="danger" />
-     <Button />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
 
-     <GlobalStyle />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }

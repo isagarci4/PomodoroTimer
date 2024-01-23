c2dfc1455d1afcc1ad88be2a51aa8db95a0d0cc8
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2024-01-23 12:28:22 -0300

    feat: adicionando datalist ao input

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,15 +17,25 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             type="text"
             placeholder="Dê um nome para o seu projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
